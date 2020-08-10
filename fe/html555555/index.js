052e41b70a6b05cee6f1f2086b803de3542c7ee2
aioverg <1010998682@qq.com>
2020-08-10 15:46:15 +0800

    react demo

---
+++ fe/html555555/index.js
@@ -0,0 +1,8 @@
+function App(){
+	/* const name = "world"
+	const el = `<h2>hello {name}</h2>`
+	ReactDOM.render("<h2>hello</h2>", document.getElementById("root")) */
+	return ('<h2>hello</h2>')
+}
+
+export default App;
