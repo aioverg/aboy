5d4a474c54314920b3d7be78d81f57639e2df177
aioverg <aioverg@outlook.com>
2021-02-26 23:22:11 +0800

    css

---
+++ joy/data.js
@@ -0,0 +1,13 @@
+const record = [ // record 文件夹文章链接信息
+  {
+    name: '饥饿',
+    path: './record/record-hunger.html'
+  }
+]
+
+const security = [
+  {
+    name: '开始',
+    path: './security/security-start.html'
+  }
+]
